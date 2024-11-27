da48b32f543df34c898c953b899caf87b648fffb
dzania05 <pandu.whello@gmail.com>
2024-11-27 14:25:59 +0700

    add documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'client'));
   app.setGlobalPrefix('api/v1/');
   await app.listen(3000);
 }
